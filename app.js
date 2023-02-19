d9d081e5f2f52fc38bb16808cf5883309c4ad015
nkhlrgh <43105834+nkhlrgh@users.noreply.github.com>
2023-02-19 20:30:37 +0530

    feature: display searched movie

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=43fbb684b7592b8d42aa72c4aff9dcd5&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=43fbb684b7592b8d42aa72c4aff9dcd5&query=";
+const divRow = document.getElementById('movieWrapper');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
 returnMovies(APILINK);
+
+//Display discover movie list
 function returnMovies(url) {
     fetch(url).then(response => response.json())
     .then((data) => {
         data.results.forEach(movie => {
-            console.log(movie.title);
-
-            const divRow = document.getElementById('movieWrapper');
 
             const divColumn = document.createElement('div');
             divColumn.setAttribute('class', 'col-lg-3');
@@ -19,6 +21,7 @@ function returnMovies(url) {
 
             const img = document.createElement('img');
             img.setAttribute('class', 'card-img-top')
+            //Set image src
             img.src = IMG_PATH + movie.poster_path;
 
             const divCardBody = document.createElement('div');
@@ -26,6 +29,7 @@ function returnMovies(url) {
 
             const cardTitle = document.createElement('h5');
             cardTitle.setAttribute('class', 'card-title');
+            //Set movie title
             cardTitle.innerHTML = `${movie.title}`;
 
             divCardBody.appendChild(cardTitle);
@@ -36,3 +40,16 @@ function returnMovies(url) {
         })
     })
 }
+
+//Display searched movie
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    divRow.innerHTML = "";
+
+    const searchItem = search.value;
+
+    if(searchItem) {
+        returnMovies(SEARCHAPI + searchItem);
+        search.value = "";
+    }
+})
